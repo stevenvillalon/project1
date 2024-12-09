5b0e39845aba4b8e66370950b38a3e80b8c6ad5e
Steven Villalon <180530680+stevenvillalon@users.noreply.github.com>
2024-12-09 02:36:36 -0500

    Changed season to 2024-2025. Also did some cleanup on the comments in my code.

--- project1_streamlit_app.py
+++ project1_streamlit_app.py
@@ -10,16 +10,16 @@ from nba_api.stats.endpoints import playergamelog
 teams_df = pd.read_csv('nba_teams.csv')
 players_df = pd.read_csv('nba_players.csv')
 
-# Streamlit app title
+# Streamlit app header
 st.title("Histograms and Hoops 🏀")
 st.subheader("What are the chances your favorite NBA player will score X points?")
 st.write("This question comes up often when watching games with friends. Using some basic statistical tools, we can actually answer this question...for points and a number of other basketball stats.")
-st.markdown("<p style='color:red;'>This tool uses data from the 2023-2024 NBA regular season. I will update with data from the current season when teams have played more games.</p>", unsafe_allow_html=True)
+st.markdown("<p style='color:yellow;'>UPDATE! This tool is now using data from the 2024-2025 NBA regular season.</p>", unsafe_allow_html=True)
 st.markdown("Created by: [Steven Villalon](mailto:steven.villalon@gmail.com)  \nSource: NBA API")
 st.markdown("---")
 
 
-# Step 1: User selects an NBA team
+# Step 1: Select an NBA team
 teams_df = teams_df.sort_values('full_name')
 team_names = teams_df['full_name'].tolist()
 selected_team = st.selectbox("Select a Team", team_names, index=team_names.index("Los Angeles Lakers"))
@@ -29,17 +29,21 @@ team_id = teams_df[teams_df['full_name'] == selected_team]['id'].values[0]
 filtered_players = players_df[players_df['TeamID'] == team_id].sort_values('PLAYER')
 player_names = filtered_players['PLAYER'].tolist()
 
-# Step 2: User selects a player
+
+# Step 2: Select a player
 default_player = player_names[0] if "LeBron James" not in player_names else "LeBron James"
 selected_player = st.selectbox("Select a Player", player_names, index=player_names.index(default_player))
 
 # Get the player ID for the selected player
 player_id = filtered_players[filtered_players['PLAYER'] == selected_player]['PLAYER_ID'].values[0]
 
-# Step 3: Get Player Logs for Last Season
-game_logs = playergamelog.PlayerGameLog(player_id=player_id, season='2023', season_type_all_star='Regular Season')
+
+# Step 3: Get player logs for selected player
+game_logs = playergamelog.PlayerGameLog(player_id=player_id, season='2024', season_type_all_star='Regular Season')
 game_logs_df = game_logs.get_data_frames()[0]
 
+
+# Step 4: Select a KPI of interest
 # Check if there is data available
 if game_logs_df.empty:
     st.write("No data available for the selected player.")
@@ -75,15 +79,17 @@ else:
     # Map the user's selection back to the original statistic code
     statistic = {v: k for k, v in stat_names.items()}[selected_readable_stat]
 
-    # Plot the histogram
+    
+    # Step 5: Plot the histogram
     plt.figure(figsize=(10, 6))
     plt.hist(game_logs_df[statistic], bins=10, edgecolor='black')
-    plt.title(f"{selected_player} - Distribution of {stat_names[statistic]} (2023-2024 Regular Season)")
+    plt.title(f"{selected_player} - Distribution of {stat_names[statistic]} (2024-2025 Regular Season)")
     plt.xlabel(stat_names[statistic])
     plt.ylabel("Number of Games")
     st.pyplot(plt)
 
-    # Calculate and display key statistics
+   
+    # Step 6: Calculate and display key statistics
     mean_stat = round(np.mean(game_logs_df[statistic]), 1)
     median_stat = round(np.median(game_logs_df[statistic]), 1)
     min_stat = np.min(game_logs_df[statistic])
@@ -99,9 +105,10 @@ else:
     st.write(f"Minimum {stat_names[statistic]} = {min_stat}")
     st.write(f"Maximum {stat_names[statistic]} = {max_stat}")
 
-    # Step 5: Add Confidence Intervals
+    
+    # Step 7: Add Confidence Intervals
     # Calculate the Margin of Error (MOE)
-    if total_games >= 30:
+    if total_games >= 20:
         alpha = 0.05
         deg_freedom = total_games - 1
         critical_value = t.ppf(1 - (alpha / 2), deg_freedom)
@@ -113,15 +120,16 @@ else:
 
         st.markdown(f"**<h5 style='color:green'>In the short-term (2-3 games), we are 95% confident that {selected_player} will average between {lower_bound} and {upper_bound} {stat_names[statistic]} per game.</h5>**", unsafe_allow_html=True)
     else:
-        st.markdown(f'<p style="color:red;">Not enough games to calculate a valid prediction interval.</p>', unsafe_allow_html=True)
+        st.markdown(f'<p style="color:red;">Not enough games played to calculate a valid prediction interval.</p>', unsafe_allow_html=True)
 
 
-    # Step 6: Add a number input box
+    # Step 8: Make a prediction
+    # Add a number input box
     st.header("Make a Prediction")
     user_input = st.number_input(f"Enter {stat_names[statistic]} value to calculate a prediction:", value = 25, min_value=0, step=1)
 
 
-    # Step 7: Calculate the quantile of the user input
+    # Calculate the quantile of the user input
     percent_below = percentileofscore(game_logs_df[statistic], user_input, kind = "strict")
     prediction = 100 - percent_below
 
